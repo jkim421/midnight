cf99b899c709394eef5bde662a00b223fb2bb055
john.kim <jkim21@pm.me>
2019-01-21 14:37:58 -0500

    Rename results list files

---
+++ frontend/components/results_list/results_list.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ResultsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      page: 1,
+      list: [],
+      searching: false,
+    };
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default ResultsList;
---
+++ frontend/components/results_list/results_list_container.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ResultsList from './results_list';
+
+const msp = state => {
+  return {
+
+  };
+};
+
+const mdp = dispatch => {
+  return {
+
+  };
+};
+
+export default connect(null, mdp)(SearchBar);
