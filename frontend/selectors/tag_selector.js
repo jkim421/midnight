8f2cc3f0a27dada4d995e10c5c4ec5357452c8fc
john.kim <jkim21@pm.me>
2019-01-22 12:20:58 -0500

    Build tag selectors and constants

---
+++ frontend/selectors/tag_selector.js
@@ -0,0 +1,42 @@
+import { GENRES, TYPES } from './defined_tags.js';
+
+export const parseTags = (string) => {
+  let parsed;
+  let split = string.split(", ");
+
+  const score = parseScore(split);
+  const genres = parseGenres(split);
+
+  return {
+    score: score,
+    genres: genres,
+  };
+};
+
+const parseScore = (arr) => {
+  let score = null;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].slice(0, 6) === "Score:") {
+      score = parseFloat(arr[i].slice(7));
+    }
+  }
+
+  return score;
+};
+
+const parseGenres = (arr) => {
+  let genreTags = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (GENRES.includes(arr[i])) {
+      genreTags.push(arr[i]);
+    }
+  }
+
+  return genreTags;
+};
+
+const parseYear = (string) => {
+  return string.slice(0, 4);
+};
