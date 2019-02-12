fdf6e39a5c694c14d56a168ed72b5016e8e9be6d
john.kim <jkim21@pm.me>
2019-02-12 02:07:21 -0500

    Build dynamic sort symbols, refactor sort display methiods

--- frontend/components/sort_bar/sort_bar.jsx
+++ frontend/components/sort_bar/sort_bar.jsx
@@ -4,13 +4,58 @@ class SortBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      title: 1,
+      score: 4,
+      start: 6,
+      end: 8,
     }
   }
 
-  selectedSort(sortNum) {
-    if (this.props.sort === sortNum) {
+  currentSort(sortNum) {
+    if (this.props.sort === sortNum || this.props.sort === sortNum + 1) {
       return "SortBar-option-selected";
+    }
+  }
+
+  renderTitle() {
+    if (this.state.title === 1) {
+      return "A-Z";
+    } else {
+      return "Z-A";
+    }
+  }
+
+  renderSymbol(type, num) {
+    if (this.state[type] === num) {
+      return "\u2191";
+    } else {
+      return "\u2193";
+    }  }
+
+  handleSort(type) {
+    const sort = this.props.sort;
+
+    if (type === "title") {
+      this.selectSort(type, 1, 2);
+    } else if (type === "score") {
+      this.selectSort(type, 3, 4);
+    } else if (type === "start") {
+      this.selectSort(type, 5, 6);
+    } else if (type === "end") {
+      this.selectSort(type, 7, 8);
+    }
+  }
+
+  selectSort(type, a, b) {
+    const sort = this.props.sort;
+    if (sort === a || sort === b) {
+      sort === a ?
+        this.setState({[type]: b}, () => this.props.updateSort(b)) :
+        this.setState({[type]: a}, () => this.props.updateSort(a));
+    } else {
+      this.state[type] === a ?
+        this.setState({[type]: a}, () => this.props.updateSort(a)) :
+        this.setState({[type]: b}, () => this.props.updateSort(b));
     }
   }
 
@@ -18,52 +63,48 @@ class SortBar extends React.Component {
     return (
       <div className="SortBar-container">
         <div
-          className={`SortBar-option ${ this.selectedSort(1) }`}
-          onClick={ () => this.props.updateSort(1) }
+          className={`SortBar-option ${ this.currentSort(1) }`}
+          onClick={ () => this.handleSort("title") }
         >
-          A-Z
+          <span className="SortBar-option-type">
+            Title
+          </span>
+          <span className="SortBar-option-sym">
+            { this.renderTitle() }
+          </span>
         </div>
         <div
-          className={`SortBar-option ${ this.selectedSort(2) }`}
-          onClick={ () => this.props.updateSort(2) }
+          className={`SortBar-option ${ this.currentSort(3) }`}
+          onClick={ () => this.handleSort("score") }
         >
-          Z-A
+          <span className="SortBar-option-type">
+            Score
+          </span>
+          <span className="SortBar-option-sym">
+            { this.renderSymbol("score", 3) }
+          </span>
         </div>
         <div
-          className={`SortBar-option ${ this.selectedSort(3) }`}
-          onClick={ () => this.props.updateSort(3) }
+          className={`SortBar-option ${ this.currentSort(5) }`}
+          onClick={ () => this.handleSort("start") }
         >
-          Score Asc
+          <span className="SortBar-option-type">
+            Start Yr
+          </span>
+          <span className="SortBar-option-sym">
+            { this.renderSymbol("start", 5) }
+          </span>
         </div>
         <div
-          className={`SortBar-option ${ this.selectedSort(4) }`}
-          onClick={ () => this.props.updateSort(4) }
+          className={`SortBar-option ${ this.currentSort(7) }`}
+          onClick={ () => this.handleSort("end") }
         >
-          Score Desc
-        </div>
-        <div
-          className={`SortBar-option ${ this.selectedSort(5) }`}
-          onClick={ () => this.props.updateSort(5) }
-        >
-          Start Yr Asc
-        </div>
-        <div
-          className={`SortBar-option ${ this.selectedSort(6) }`}
-          onClick={ () => this.props.updateSort(6) }
-        >
-          Start Yr Desc
-        </div>
-        <div
-          className={`SortBar-option ${ this.selectedSort(7) }`}
-          onClick={ () => this.props.updateSort(7) }
-        >
-          End Yr Asc
-        </div>
-        <div
-          className={`SortBar-option ${ this.selectedSort(8) }`}
-          onClick={ () => this.props.updateSort(8) }
-        >
-          End Yr Desc
+        <span className="SortBar-option-type">
+          End Yr
+        </span>
+        <span className="SortBar-option-sym">
+          { this.renderSymbol("end", 7) }
+        </span>
         </div>
       </div>
     )
